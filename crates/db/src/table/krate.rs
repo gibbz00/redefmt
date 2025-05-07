bd2f26f33a4ee01401d8ff60b9c1a09bd1e02bc8
gibbz00 <gabrielhansson00@gmail.com>
2025-05-07 08:31:26 +0200

    feat(db): find crate id by name

--- crates/db/src/table/krate.rs
+++ crates/db/src/table/krate.rs
@@ -20,18 +20,36 @@ pub enum CrateTableError {
 }
 
 pub trait CrateTable {
-    fn find_crate_record(&self, id: CrateId) -> Result<Option<Crate<'static>>, DbClientError>;
+    fn find_crate_by_id(&self, id: CrateId) -> Result<Option<Crate<'static>>, DbClientError>;
+
+    fn find_crate_by_name(&self, name: &CrateName<'_>) -> Result<Option<(CrateId, Crate<'static>)>, DbClientError>;
 
     fn insert_crate_record(&self, record: &Crate<'_>) -> Result<CrateId, DbClientError>;
 }
 
 impl CrateTable for DbClient<MainDb> {
-    fn find_crate_record(&self, id: CrateId) -> Result<Option<Crate<'static>>, DbClientError> {
+    fn find_crate_by_id(&self, id: CrateId) -> Result<Option<Crate<'static>>, DbClientError> {
         let krate = self
             .connection
             .query_row("SELECT name FROM crate WHERE id = ?1", [id], |res| res.get(0))
             .optional()?
             .map(|name| Crate { name });
+
+        Ok(krate)
+    }
+
+    fn find_crate_by_name(&self, name: &CrateName<'_>) -> Result<Option<(CrateId, Crate<'static>)>, DbClientError> {
+        let krate = self
+            .connection
+            .query_row("SELECT id, name FROM crate WHERE name = ?1", [name], |res| {
+                let id = res.get(0)?;
+                // possibly redundant if name has already been given
+                let name = res.get(1)?;
+
+                Ok((id, name))
+            })
+            .optional()?
+            .map(|(id, name)| (id, Crate { name }));
 
         Ok(krate)
     }
@@ -60,31 +78,45 @@ mod tests {
     }
 
     #[test]
-    fn find_and_create() {
-        let (_dir_guard, client) = DbClient::mock_db();
+    fn insert_and_find_by_id() {
+        let (_dir_guard, db) = DbClient::mock_db();
 
         let mock_record = mock_crate_record();
 
-        let id = client.insert_crate_record(&mock_record).unwrap();
+        let id = db.insert_crate_record(&mock_record).unwrap();
 
-        let found_record = client.find_crate_record(id).unwrap().unwrap();
+        let found_record = db.find_crate_by_id(id).unwrap().unwrap();
         assert_eq!(mock_record, found_record);
 
         let fake_id = mock_crate_id();
-        let found_record = client.find_crate_record(fake_id).unwrap();
+        let found_record = db.find_crate_by_id(fake_id).unwrap();
         assert!(found_record.is_none());
     }
 
     #[test]
     fn unique_name_index() {
-        let (_dir_guard, client) = DbClient::mock_db();
+        let (_dir_guard, db) = DbClient::mock_db();
 
         let mock_record = mock_crate_record();
 
-        let first_insert_result = client.insert_crate_record(&mock_record);
+        let first_insert_result = db.insert_crate_record(&mock_record);
         assert!(first_insert_result.is_ok());
 
-        let second_insert_result = client.insert_crate_record(&mock_record);
+        let second_insert_result = db.insert_crate_record(&mock_record);
         assert!(second_insert_result.is_err());
+    }
+
+    #[test]
+    fn find_by_name() {
+        let (_dir_guard, db) = DbClient::mock_db();
+
+        let record = mock_crate_record();
+
+        let id = db.insert_crate_record(&record).unwrap();
+
+        let actual = db.find_crate_by_name(&record.name).unwrap().unwrap();
+        let expected = (id, record);
+
+        assert_eq!(expected, actual);
     }
 }
