d5419b2193aa11d2949a2ddd2b87754ce845bdec
gibbz00 <gabrielhansson00@gmail.com>
2025-05-08 18:38:13 +0200

    feat(decoder): decode string values

---
+++ crates/common/src/codec/pointer_width.rs
@@ -0,0 +1,32 @@
+pub enum PointerWidth {
+    U16,
+    U32,
+    U64,
+}
+
+impl PointerWidth {
+    pub fn of_target() -> PointerWidth {
+        #[cfg(target_pointer_width = "16")]
+        {
+            PointerWidth::U16
+        }
+
+        #[cfg(target_pointer_width = "32")]
+        {
+            PointerWidth::U32
+        }
+
+        #[cfg(target_pointer_width = "64")]
+        {
+            PointerWidth::U64
+        }
+    }
+
+    pub fn size(&self) -> usize {
+        match self {
+            PointerWidth::U16 => 2,
+            PointerWidth::U32 => 4,
+            PointerWidth::U64 => 8,
+        }
+    }
+}
