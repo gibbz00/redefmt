a0cf98efc13f088293415d010cba5e3c02267e3d
gibbz00 <gabrielhansson00@gmail.com>
2025-05-11 21:15:28 +0200

    feat(logger): expand global register and dispatcher API

--- crates/logger/src/dispatcher.rs
+++ crates/logger/src/dispatcher.rs
@@ -1,9 +1,57 @@
-// TODO: make unsafe?
-pub trait Dispatcher {
-    fn acquire(&self);
-
-    fn write(&self, bytes: &[u8]);
-
-    /// Should flush also flush the the internal buffer
-    fn release(&self);
+mod core {
+    pub trait Dispatcher {
+        fn write(&mut self, bytes: &[u8]);
+    }
 }
+pub(crate) use core::Dispatcher;
+
+#[cfg(test)]
+mod test_dispatcher {
+    use alloc::sync::Arc;
+    use core::cell::RefCell;
+
+    use bytes::{BufMut, BytesMut};
+    use critical_section::Mutex;
+
+    use crate::*;
+
+    #[derive(Default)]
+    pub struct SimpleTestDispatcher {
+        pub bytes: BytesMut,
+    }
+
+    impl Dispatcher for SimpleTestDispatcher {
+        fn write(&mut self, bytes: &[u8]) {
+            self.bytes.put_slice(bytes);
+        }
+    }
+
+    #[derive(Clone)]
+    pub struct SharedTestDispatcher {
+        bytes: Arc<Mutex<RefCell<BytesMut>>>,
+    }
+
+    impl SharedTestDispatcher {
+        pub fn new() -> Self {
+            Self { bytes: Arc::new(Mutex::new(RefCell::new(BytesMut::new()))) }
+        }
+
+        pub fn assert_bytes(&self, expected_bytes: &[u8]) {
+            critical_section::with(|cs| {
+                let bytes_ref = self.bytes.borrow_ref(cs);
+                let actual_bytes = bytes_ref.as_ref();
+                assert_eq!(expected_bytes, actual_bytes);
+            })
+        }
+    }
+
+    impl Dispatcher for SharedTestDispatcher {
+        fn write(&mut self, bytes: &[u8]) {
+            critical_section::with(|cs| {
+                self.bytes.borrow(cs).borrow_mut().put_slice(bytes);
+            })
+        }
+    }
+}
+#[cfg(test)]
+pub(crate) use test_dispatcher::{SharedTestDispatcher, SimpleTestDispatcher};
--- crates/logger/src/lib.rs
+++ crates/logger/src/lib.rs
@@ -4,17 +4,19 @@
 // TEMP:
 #![allow(missing_docs)]
 
+extern crate alloc;
+
 mod write_value;
 pub(crate) use write_value::WriteValue;
 
 mod dispatcher;
-pub(crate) use dispatcher::Dispatcher;
+pub(crate) use dispatcher::*;
 
 mod stamper;
 pub(crate) use stamper::Stamper;
 
 mod global_registry;
-pub use global_registry::GlobalRegistry;
+pub use global_registry::{DispatcherHandle, GlobalRegistry};
 
 mod global_logger;
 pub(crate) use global_logger::GlobalLogger;
