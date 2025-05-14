b4ba5f85de21cad6ce9fc145459a1e78ffd88732
gibbz00 <gabrielhansson00@gmail.com>
2025-05-14 10:52:21 +0200

    chore(common): move type hint repr of collections to 1XX

--- crates/common/src/codec/frame/type_hint.rs
+++ crates/common/src/codec/frame/type_hint.rs
@@ -19,36 +19,36 @@ pub enum TypeHint {
     F32 = 33,
     F64 = 34,
 
-    // * Write Content ID * 1XX
-    //
-    // (Current XX = 13 repr implies u32 write_id, (u16 crate_id + u16 write_statement_id))
-    WriteId = 113,
-
     // ** Collections ** 2XX
 
     // length hint
-    StringSlice = 200,
+    StringSlice = 100,
 
     // length + type hint for each value
     // (effectively a dyn slice)
-    Tuple = 201,
+    Tuple = 101,
 
     // List and DynList reused between array, vec and slice
 
     // length + leading type hint if length > 0
-    List = 202,
+    List = 102,
     // length + type hint for each element
-    DynList = 203,
+    DynList = 103,
 
     // length + leading type hint
     // (no DynSet since Hash is not dyn compatible)
-    Set = 204,
+    Set = 104,
 
     // length + two leading type hints
-    Map = 206,
+    Map = 106,
     // length + leading type hint + type hint for each element
     // (no need for DynDynMap since Hash is not dyn compatible)
-    DynMap = 207,
+    DynMap = 107,
+
+    // * Write Content ID * 2XX
+    //
+    // (Current XX = 13 repr implies u32 write_id, (u16 crate_id + u16 write_statement_id))
+    WriteId = 213,
 }
 
 #[cfg(test)]
@@ -57,7 +57,7 @@ mod tests {
 
     #[test]
     fn write_content_id_type_hint() {
-        let write_content_section = 100;
+        let write_content_section = 200;
         let write_id_type_hint_repr = TypeHint::WriteId as u8;
         let u32_type_hint_repr = TypeHint::U32 as u8;
 
