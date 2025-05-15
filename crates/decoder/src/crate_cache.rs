b6f9b46127154916852a4ebb9478ca6affd92271
gibbz00 <gabrielhansson00@gmail.com>
2025-05-15 21:39:11 +0200

    perf(decoder): use `elsa::FrozenMap` for crate cache

--- crates/decoder/src/crate_cache.rs
+++ crates/decoder/src/crate_cache.rs
@@ -1,8 +1,6 @@
-use std::{
-    collections::{HashMap, hash_map::Entry},
-    path::Path,
-};
+use std::path::Path;
 
+use elsa::sync::FrozenMap;
 use redefmt_common::identifiers::CrateId;
 use redefmt_db::{CrateDb, DbClient, MainDb, Table, crate_table::Crate};
 
@@ -10,35 +8,37 @@ use crate::RedefmtDecoderError;
 
 pub type CrateValue = (DbClient<CrateDb>, Crate<'static>);
 
-#[derive(Default)]
 pub struct CrateCache {
-    map: HashMap<CrateId, CrateValue>,
+    map: FrozenMap<CrateId, Box<CrateValue>>,
 }
 
 impl CrateCache {
-    pub fn fetch_and_save(
-        &mut self,
+    // TEMP:
+    #[allow(clippy::new_without_default)]
+    pub fn new() -> Self {
+        Self { map: FrozenMap::new() }
+    }
+
+    pub fn get_or_insert(
+        &self,
         id: CrateId,
         db: &DbClient<MainDb>,
         state_dir: &Path,
     ) -> Result<&CrateValue, RedefmtDecoderError> {
-        if let Entry::Vacant(vacant_entry) = self.map.entry(id) {
-            let Some(crate_record) = db.find_by_id(id)? else {
-                return Err(RedefmtDecoderError::UnknownCrate(id));
-            };
+        let value = match self.map.get(&id) {
+            Some(value) => value,
+            None => {
+                let Some(crate_record) = db.find_by_id(id)? else {
+                    return Err(RedefmtDecoderError::UnknownCrate(id));
+                };
 
-            let crate_db = DbClient::new_crate(state_dir, crate_record.name())?;
+                let crate_db = DbClient::new_crate(state_dir, crate_record.name())?;
 
-            vacant_entry.insert((crate_db, crate_record));
-        }
+                self.map.insert(id, Box::new((crate_db, crate_record)))
+            }
+        };
 
-        let crate_value = self.map.get(&id).expect("crate value not stored in cache");
-
-        Ok(crate_value)
-    }
-
-    pub fn get(&self, id: CrateId) -> Option<&CrateValue> {
-        self.map.get(&id)
+        Ok(value)
     }
 }
 
@@ -47,7 +47,7 @@ mod ext {
     use super::*;
 
     impl CrateCache {
-        pub fn inner(&self) -> &HashMap<CrateId, CrateValue> {
+        pub fn inner(&self) -> &FrozenMap<CrateId, Box<CrateValue>> {
             &self.map
         }
     }
