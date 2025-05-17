2d3eb064048bdc98d41e2d161359021c4819c79a
gibbz00 <gabrielhansson00@gmail.com>
2025-05-17 14:15:00 +0200

    refactor(decoder): decode segments independent of print statement

---
+++ crates/decoder/src/list_context.rs
@@ -0,0 +1,104 @@
+use redefmt_common::codec::frame::{PointerWidth, TypeHint};
+use tokio_util::bytes::{Buf, BytesMut};
+
+use crate::*;
+
+pub struct ListValueContext<'caches> {
+    pointer_width: PointerWidth,
+    expected_length: usize,
+    buffer: Vec<Value>,
+    element_context: Option<Box<ValueContext<'caches>>>,
+    element_type_hint: Option<TypeHint>,
+}
+
+impl<'caches> ListValueContext<'caches> {
+    pub fn new(pointer_width: PointerWidth, expected_length: usize) -> Self {
+        Self {
+            pointer_width,
+            expected_length,
+            buffer: Vec::with_capacity(expected_length),
+            element_context: Default::default(),
+            element_type_hint: None,
+        }
+    }
+
+    pub fn decode_list(
+        &mut self,
+        stores: &DecoderStores<'caches>,
+        src: &mut BytesMut,
+    ) -> Result<Option<Vec<Value>>, RedefmtDecoderError> {
+        let Some(element_type_hint) = self.get_or_insert_element_type_hint(src)? else {
+            return Ok(None);
+        };
+
+        while self.buffer.len() < self.expected_length {
+            let element_context = self
+                .element_context
+                .get_or_insert_with(|| Box::new(ValueContext::new(self.pointer_width, element_type_hint)));
+
+            match element_context.decode_value(stores, src)? {
+                Some(value) => {
+                    self.buffer.push(value);
+                    self.element_context = None;
+                }
+                None => return Ok(None),
+            }
+        }
+
+        let values = std::mem::take(&mut self.buffer);
+
+        // No need to clear value_context.list_value_context, decode value should be done here
+
+        Ok(Some(values))
+    }
+
+    pub fn decode_dyn_list(
+        &mut self,
+        stores: &DecoderStores<'caches>,
+        src: &mut BytesMut,
+    ) -> Result<Option<Vec<Value>>, RedefmtDecoderError> {
+        while self.buffer.len() < self.expected_length {
+            let Some(element_type_hint) = self.get_or_insert_element_type_hint(src)? else {
+                return Ok(None);
+            };
+
+            let element_context = self
+                .element_context
+                .get_or_insert_with(|| Box::new(ValueContext::new(self.pointer_width, element_type_hint)));
+
+            match element_context.decode_value(stores, src)? {
+                Some(value) => {
+                    self.buffer.push(value);
+
+                    // Clear element context
+                    self.element_type_hint = None;
+                    self.element_context = None;
+                }
+                None => return Ok(None),
+            }
+        }
+
+        let values = std::mem::take(&mut self.buffer);
+
+        // No need to clear value_context.list_value_context, decode value should be done here
+
+        Ok(Some(values))
+    }
+
+    fn get_or_insert_element_type_hint(&mut self, src: &mut BytesMut) -> Result<Option<TypeHint>, RedefmtDecoderError> {
+        if let Some(type_hint) = self.element_type_hint {
+            return Ok(Some(type_hint));
+        }
+
+        let Ok(type_hint_repr) = src.try_get_u8() else {
+            return Ok(None);
+        };
+
+        let type_hint =
+            TypeHint::from_repr(type_hint_repr).ok_or(RedefmtDecoderError::UnknownTypeHint(type_hint_repr))?;
+
+        self.element_type_hint = Some(type_hint);
+
+        Ok(Some(type_hint))
+    }
+}
