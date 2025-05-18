f163a228110d5d2b8bf2e3a3d3f9587e61d5bbc7
gibbz00 <gabrielhansson00@gmail.com>
2025-05-18 10:27:17 +0200

    chore(decoder): place sub decoders in a dedicated module

--- crates/decoder/src/lib.rs
+++ crates/decoder/src/lib.rs
@@ -18,20 +18,8 @@ pub(crate) use cache::*;
 mod frame;
 pub(crate) use frame::*;
 
-mod segment_decoder;
-pub(crate) use segment_decoder::{DecodedSegment, SegmentsDecoder};
-
-mod value_decoder;
-pub(crate) use value_decoder::ValueDecoder;
-
-mod list_decoder;
-pub(crate) use list_decoder::ListValueDecoder;
-
-mod write_statement_decoder;
-pub(crate) use write_statement_decoder::WriteStatementDecoder;
-
-mod type_structure_decoder;
-pub(crate) use type_structure_decoder::TypeStructureDecoder;
+mod sub_decoders;
+pub(crate) use sub_decoders::*;
 
 mod utils;
 pub(crate) use utils::DecoderUtils;
