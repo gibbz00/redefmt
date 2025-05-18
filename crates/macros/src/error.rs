62a90705abde3201673cbc464c2d3fa7e6f89c91
gibbz00 <gabrielhansson00@gmail.com>
2025-05-18 09:21:17 +0200

    feat(macros): db client setup

---
+++ crates/macros/src/error.rs
@@ -0,0 +1,22 @@
+use std::env::VarError;
+
+use proc_macro2::Span;
+use redefmt_db::{DbClientError, StateDirError, crate_table::CrateNameError};
+
+#[derive(Debug, thiserror::Error)]
+pub enum RedefmtMacroError {
+    #[error("state directory resolution error")]
+    StateDir(#[from] StateDirError),
+    #[error("internal database failure")]
+    Db(#[from] DbClientError),
+    #[error("failed to retrieve crate name from '$CARGO_PKG_NAME'")]
+    CrateNameEnv(#[from] VarError),
+    #[error("invalid crate name")]
+    CrateName(#[from] CrateNameError),
+}
+
+impl RedefmtMacroError {
+    pub fn as_syn_error(&self, span: Span) -> syn::Error {
+        syn::Error::new(span, self.to_string())
+    }
+}
