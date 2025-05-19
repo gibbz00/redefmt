3c71cc0e3d4858c12923640d0669f015a109fe54
gibbz00 <gabrielhansson00@gmail.com>
2025-05-19 07:01:58 +0200

    feat(macros): declare wire and print macros

--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -13,7 +13,31 @@ pub(crate) use db_client::DbClients;
 
 mod derive_format;
 
+mod write_statement;
+
+mod print_statement;
+
 #[proc_macro_derive(Format, attributes(redefmt))]
 pub fn derive_format(token_stream: TokenStream) -> TokenStream {
     derive_format::macro_impl(token_stream)
+}
+
+#[proc_macro]
+pub fn write(token_stream: TokenStream) -> TokenStream {
+    write_statement::macro_impl(token_stream, false)
+}
+
+#[proc_macro]
+pub fn writeln(token_stream: TokenStream) -> TokenStream {
+    write_statement::macro_impl(token_stream, true)
+}
+
+#[proc_macro]
+pub fn print(token_stream: TokenStream) -> TokenStream {
+    print_statement::macro_impl(token_stream, false)
+}
+
+#[proc_macro]
+pub fn println(token_stream: TokenStream) -> TokenStream {
+    print_statement::macro_impl(token_stream, true)
 }
