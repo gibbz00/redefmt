8c41ff0a60ab259426f618ee239d79e394f2aaeb
gibbz00 <gabrielhansson00@gmail.com>
2025-05-19 08:20:15 +0200

    chore(args): move `argument_register` module under `format_string`

--- crates/args/src/format_string/mod.rs
+++ crates/args/src/format_string/mod.rs
@@ -4,3 +4,7 @@ pub(crate) use core::FormatStringSegment;
 
 mod parse;
 pub use parse::FormatStringParseError;
+
+mod argument_register;
+pub(crate) use argument_register::ArgumentRegister;
+pub use argument_register::{RequiredArguments, RequiredArgumentsError};
