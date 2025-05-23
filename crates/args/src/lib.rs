67947dedd833b1d81a39cba1fbd3dc2694386847
gibbz00 <gabrielhansson00@gmail.com>
2025-05-23 14:28:11 +0200

    refactor(args): split into an internal crate

--- crates/args/src/lib.rs
+++ crates/args/src/lib.rs
@@ -4,33 +4,4 @@
 // TEMP:
 #![allow(missing_docs)]
 
-extern crate alloc;
-
-mod error;
-pub use error::ParseError;
-
-mod integer;
-pub(crate) use integer::Integer;
-
-pub mod identifier;
-pub(crate) use identifier::{Identifier, IdentifierParseError};
-
-mod format_argument;
-pub(crate) use format_argument::FormatArgument;
-
-mod format_options;
-pub use format_options::FormatOptions;
-pub(crate) use format_options::*;
-
-mod format_segment;
-pub(crate) use format_segment::{FormatSegment, FormatStringSegment};
-
-mod format_string;
-pub use format_string::{FormatString, FormatStringParseError};
-
-#[cfg(feature = "provided-args")]
-pub mod provided_args;
-#[cfg(feature = "provided-args")]
-pub(crate) use provided_args::*;
-
-mod serde_utils;
+pub use redefmt_args_internal::*;
---
+++ crates/args_internal/src/lib.rs
@@ -0,0 +1,35 @@
+//! # `redefmt-args-internal`
+
+#![no_std]
+// TEMP:
+#![allow(missing_docs)]
+
+extern crate alloc;
+
+mod error;
+pub use error::ParseError;
+
+mod integer;
+pub(crate) use integer::Integer;
+
+mod identifier;
+pub(crate) use identifier::{Identifier, IdentifierParseError};
+
+mod format_argument;
+pub(crate) use format_argument::FormatArgument;
+
+mod format_options;
+pub(crate) use format_options::*;
+
+mod format_segment;
+pub(crate) use format_segment::{FormatSegment, FormatStringSegment};
+
+mod format_string;
+pub use format_string::{FormatString, FormatStringParseError};
+
+#[cfg(feature = "provided-args")]
+pub mod provided_args;
+#[cfg(feature = "provided-args")]
+pub(crate) use provided_args::*;
+
+mod serde_utils;
