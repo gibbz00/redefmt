9a26c799bccdca00750c0fec7bd7f720cdb997c2
gibbz00 <gabrielhansson00@gmail.com>
2025-06-01 16:59:40 +0200

    refactor(macros): dry up format expression dissolving

--- crates/macros/src/statement_utils.rs
+++ crates/macros/src/statement_utils.rs
@@ -1,20 +1,24 @@
-use redefmt_args::{deferred::DeferredFormatString, identifier::AnyIdentifier};
+use redefmt_args::FormatExpression;
 use redefmt_db::statement_table::stored_format_expression::StoredFormatExpression;
 
 pub struct StatementUtils;
 
 impl StatementUtils {
-    pub fn prepare_stored<'a>(
-        format_string: DeferredFormatString<'a>,
-        expressions_count: usize,
-        provided_identifiers: Vec<AnyIdentifier<'a>>,
+    pub fn dissolve_expression<'a>(
+        format_expression: FormatExpression<'a, syn::Expr>,
         append_newline: bool,
-    ) -> StoredFormatExpression<'a> {
-        StoredFormatExpression {
+    ) -> (StoredFormatExpression<'a>, Vec<syn::Expr>) {
+        let (format_string, provided_args) = format_expression.dissolve();
+
+        let (provided_args, provided_identifiers) = provided_args.dissolve_expressions();
+
+        let stored_expression = StoredFormatExpression {
             format_string,
             append_newline,
-            expected_positional_arg_count: expressions_count - provided_identifiers.len(),
+            expected_positional_arg_count: provided_args.len() - provided_identifiers.len(),
             expected_named_args: provided_identifiers,
-        }
+        };
+
+        (stored_expression, provided_args)
     }
 }
