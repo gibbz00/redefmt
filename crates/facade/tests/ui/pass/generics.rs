92ebf7e0a8cf2230323fb26326b658dcc78bbb5c
gibbz00 <gabrielhansson00@gmail.com>
2025-06-02 16:29:00 +0200

    perf: move ui tests to one end_to_end test

--- crates/facade/tests/ui/pass/generics.rs
+++
@@ -1,7 +0,0 @@
-#[derive(redefmt::Format)]
-struct Foo<T>(T);
-
-fn main() {
-    redefmt::println!("{}", Foo(1));
-    redefmt::println!("{}", Foo("x"));
-}
--- crates/facade/tests/ui/pass/tuple_struct.rs
+++
@@ -1,7 +0,0 @@
-#[derive(redefmt::Format)]
-struct Foo();
-
-#[derive(redefmt::Format)]
-struct Bar(Foo, Foo);
-
-fn main() {}
