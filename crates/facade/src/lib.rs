00933dbe9289cf07ceeea720ef4ff0371aefd6af
gibbz00 <gabrielhansson00@gmail.com>
2025-06-05 20:06:15 +0200

    feat(facade): place everything behind `deferred-*` feature flags

--- crates/facade/src/lib.rs
+++ crates/facade/src/lib.rs
@@ -4,23 +4,25 @@
 // TEMP:
 #![allow(missing_docs)]
 
-#[cfg(feature = "alloc")]
-extern crate alloc;
-
-#[cfg(feature = "macros")]
-pub use redefmt_macros::Format;
-//
-#[cfg(feature = "macros")]
-pub use redefmt_macros::{write, writeln};
-//
-mod export {
-    #[doc(hidden)]
-    pub use redefmt_core::identifiers;
-    pub use redefmt_core::{Format, Formatter};
-    #[cfg(feature = "logger")]
-    pub use redefmt_core::{codec::frame::Level, logger};
+#[cfg(feature = "deferred")]
+mod deferred {
+    #[cfg(feature = "deferred-macros")]
+    pub use redefmt_macros::Format;
+    //
+    #[cfg(feature = "deferred-macros")]
+    pub use redefmt_macros::{write, writeln};
+    //
+    mod export {
+        #[doc(hidden)]
+        pub use redefmt_core::identifiers;
+        pub use redefmt_core::{Format, Formatter};
+        #[cfg(feature = "deferred-logger")]
+        pub use redefmt_core::{codec::frame::Level, logger};
+    }
+    pub use export::*;
+    //
+    #[cfg(feature = "deferred-logger")]
+    pub use redefmt_macros::{debug, error, info, log, print, println, trace, warn};
 }
-pub use export::*;
-//
-#[cfg(feature = "logger")]
-pub use redefmt_macros::{debug, error, info, log, print, println, trace, warn};
+#[cfg(feature = "deferred")]
+pub use deferred::*;
